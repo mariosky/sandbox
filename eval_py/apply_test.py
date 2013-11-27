a213f25677f65ff9224de86694abf616b5624009
Mario Garcia Valdez <mariosky@gmail.com>
2013-11-27 09:40:15 -0800

    Inititial Sandbox, not secure yet

---
+++ eval_py/apply_test.py
@@ -0,0 +1,102 @@
+__author__ = 'mariosky'
+
+import shutil, os, tempfile
+import subprocess
+
+TIMEOUT = 5
+
+def exec_sandbox(code, test):
+    try:
+        code = code + test
+        tmp_dir = tempfile.mkdtemp()
+        tmp_script = open(os.path.join(tmp_dir, "script.py"),'w') # script.py is the name of the code passed in
+        tmp_script.write(code)
+        tmp_script.close()
+        script_path = os.path.join(tmp_dir, "script.py")
+        result = [],""
+        try:
+            out = subprocess.check_output(['python',script_path, '--timeout',str(TIMEOUT)], stderr=subprocess.STDOUT)
+            result = (output_as_list(out),0)
+
+        except subprocess.CalledProcessError , e:
+            result = output_as_list(e.output), e.returncode
+
+        finally:
+            shutil.rmtree(tmp_dir)
+        return result
+    except Exception, e:
+        return ['Error, could not evaluate'], e
+
+def output_as_list(test_output):
+    res = []
+    if not test_output:
+        return res
+
+    for l in test_output.split('\n'):
+        if len(l)>0  and l[:3] != '===' and l[:3] != '---' and 'Traceback' not in l and l[:2] != '  ':
+            res.append(l)
+    return res
+
+
+
+code = '''
+def foo():
+    print 'hello world!'
+
+
+def solution(nums):
+    if nums > 0 :
+        nums.sort()
+        return [1,2]
+        #return nums
+
+    else:
+        return [1]
+
+
+def hang():
+    while True:
+        pass
+'''
+
+test = '''
+
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_order(self):
+        self.assertEqual(solution([2,6,1,5]),[1,2,5,6])
+
+    def test_none(self):
+        self.assertEqual(solution(None),[])
+
+    #def test_hang(self):
+    #    self.assertEqual(hang(), None)
+
+class TestFoo(unittest.TestCase):
+    def test_foo(self):
+        from StringIO import StringIO
+
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            foo()
+            output = out.getvalue().strip()
+
+            assert output == 'hello world!'
+        finally:
+            sys.stdout = saved_stdout
+            print output
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(Test)
+test_result = unittest.TextTestRunner(descriptions=False, verbosity=0, stream=sys.stderr).run(suite)
+'''
+if __name__ == '__main__':
+    out = exec_sandbox(code,test)
+
+
---
+++ test.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify ,json
+
+from eval_py.apply_test import exec_sandbox
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('test_execute.html')
+
+
+@app.route('/_execute', methods=['POST'])
+def execute():
+    rpc = request.json
+    code = rpc["params"][0]
+    out = exec_sandbox(code,test)
+    return jsonify(result=out)
+
+test = '''
+import unittest, sys, json
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_order(self):
+        self.assertEqual(solution([2,6,1,5]),[1,2,5,6])
+
+    def test_none(self):
+        self.assertEqual(solution(None),[])
+
+    #def test_hang(self):
+    #    self.assertEqual(hang(), None)
+
+class TestFoo(unittest.TestCase):
+    def test_foo(self):
+        from StringIO import StringIO
+
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            foo()
+            output = out.getvalue().strip()
+
+            assert output == 'hello world!'
+        finally:
+            sys.stdout = saved_stdout
+            print output
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(Test)
+test_result = unittest.TextTestRunner(descriptions=False, verbosity=0, stream=sys.stderr).run(suite)
+'''
+
+
+if __name__ == "__main__":
+    app.run()
