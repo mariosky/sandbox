578ad8e4796b64749d33fbf669bf9f73da47647d
Mario Garcia Valdez <mariosky@gmail.com>
2013-12-10 08:20:39 -0800

    execute_sandboxed

--- test.py
+++ test.py
@@ -57,7 +57,10 @@ def execute_sand(code=None):
     url = "http://127.0.0.1:%s/_execute" % (port)
     while dC.logs(cont) == "":
         print "waiting..."
+
+
     r = requests.post(url, data=json.dumps(data), headers=headers)
+    print r
     return r.json()
 
 
