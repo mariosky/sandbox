ff37c091e19448061f368f94e47545033072019e
Mario Garcia Valdez <mariosky@gmail.com>
2015-06-23 15:35:49 -0700

    test_java

---
+++ tester/test_java.py
@@ -0,0 +1,89 @@
+__author__ = 'mariosky'
+# -*- coding: utf-8 -*-
+import shutil
+import os
+import tempfile
+import subprocess
+import json
+
+
+def run_test(code, test, type=None):
+    try:
+        java_class = test[2:test.index('\n')-len('Test')]
+        code = unicode(code)
+        test = unicode(code)
+
+
+        tmp_dir = tempfile.mkdtemp()
+        print tmp_dir
+        tmp_program = open(os.path.join(tmp_dir, "%s.java" % java_class ),'w')
+        tmp_program.write(code.encode('utf8'))
+        tmp_program.close()
+        result = [],0
+
+
+        tmp_test = open(os.path.join(tmp_dir, "%sTest.class" % java_class ),'w')
+        tmp_test.write(test.encode('utf8'))
+        tmp_test.close()
+
+
+
+        #COMPILE Program
+        try:
+            out = subprocess.check_output(['javac',os.path.join(tmp_dir,"%s.java" % java_class)], stderr=subprocess.STDOUT)
+            print 'Compile',result
+            result = (out,0)
+        except subprocess.CalledProcessError , e:
+            result = (json.dumps({ 'successes':[],'failures':[], 'errors': e.output.split('\n'), 'stdout': "", 'result': "Failure"}),e.returncode)
+            return result
+
+        #COMPILE Test
+        try:
+            out = subprocess.check_output(['javac','-cp','.:/usr/share/java/junit4.jar', os.path.join(tmp_dir, "%sTest.class" % java_class)], stderr=subprocess.STDOUT)
+            result = (out,0)
+        except subprocess.CalledProcessError , e:
+            result = (json.dumps({ 'successes':[],'failures':[], 'errors': e.output.split('\n'), 'stdout': "", 'result': "Failure"}),e.returncode)
+            return result
+
+
+        #TEST
+        try:
+            out = subprocess.check_output(['java','-cp', '.:/usr/share/java/junit4.jar','org.junit.runner.JUnitCore',os.path.join(tmp_dir, "%sTest" % java_class)], stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError , e:
+            result =  (result, e.returncode)
+        finally:
+            shutil.rmtree(tmp_dir)
+
+        return result
+    except Exception, e:
+        return ["Error, could not evaluate"], e
+
+
+
+
+
+test =r"""//CalculatorTest
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}"""
+
+
+code =r"""
+public class Calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
+  }
+}"""
+
+print run_test(code,test)
