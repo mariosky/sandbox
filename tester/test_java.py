a21b29b750eec0a7ee6f1fb762c49e59188ad259
Mario Garcia Valdez <mariosky@gmail.com>
2015-06-24 10:11:57 -0700

    test_java

--- tester/test_java.py
+++ tester/test_java.py
@@ -116,7 +116,7 @@ public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
     for (String summand: expression.split("\\+"))
-      sum -= Integer.valueOf(summand);
+      sum += Integer.valueOf(summand);
     return sum;
   }
 }"""
