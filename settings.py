42ee0d0dbaf192f4815114ed9f5d3bfb234d9ad1
mariosky <mariosky@gmail.com>
2017-06-20 12:36:06 -0700

    New name for images

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 
 
 settings = {
-"csharp": {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-test-csharp:latest',"containers": 1},
-"python": {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-test-python:latest',"containers": 1},
-"java"  : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-test-java:latest'  ,"containers": 1},
-"perl6" : {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-test-perl6:latest' ,"containers": 1},
-"go"    : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-test-go:latest',"containers": 1},
+"csharp": {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-csharp:latest',"containers": 1},
+"python": {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-python:latest',"containers": 1},
+"java"  : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-java:latest'  ,"containers": 1},
+"perl6" : {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-perl6:latest' ,"containers": 1},
+"go"    : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-go:latest',"containers": 1},
 
 }
 
