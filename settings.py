7570eccf290fdd820a6d6aacc5b0363062ff13a3
Mario Garcia Valdez <mariosky@gmail.com>
2017-06-24 14:48:49 -0700

    Default Settings

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 
 
 settings = {
-#"csharp": {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-csharp:latest',"containers": 1},
-#"python": {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-python:latest',"containers": 1},
-#"java"  : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-java:latest'  ,"containers": 1},
-"perl6" : {"command": "python /home/sandbox/worker.py", "image": 'mariosky/base-perl6:latest' ,"containers": 1},
-#"golang"    : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-go:latest',"containers": 1},
+"csharp": {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-csharp:latest',"containers": 1},
+"python": {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-python:latest',"containers": 1},
+"java"  : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-java:latest'  ,"containers": 1},
+"perl6" : {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-perl6:latest' ,"containers": 1},
+"golang"    : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-go:latest',"containers": 1},
 
 }
 
