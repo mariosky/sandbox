90b583eab4d3815fa86675dfed58023931543fc0
Mario Garcia Valdez <mariosky@gmail.com>
2017-06-24 15:45:19 -0700

    golang image

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ settings = {
 "python": {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-python:latest',"containers": 1},
 "java"  : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-java:latest'  ,"containers": 1},
 "perl6" : {"command": "python /home/sandbox/worker.py", "image": 'mariosky/sandbox-perl6:latest' ,"containers": 1},
-"golang"    : {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-go:latest',"containers": 1},
+"golang": {"command" :"python /home/sandbox/worker.py", "image": 'mariosky/sandbox-golang:latest',"containers": 1},
 
 }
 
